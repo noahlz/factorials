7e025eeb1d818680f7c889db529066bff562cde7
noahlz <nzucker@gmail.com>
2012-09-09 23:13:38 -0400

    Normalized factorial parameter to "n" and fixed bug in javainterop function.

--- src/main/clojure/factorials/core.clj
+++ src/main/clojure/factorials/core.clj
@@ -16,8 +16,8 @@
 ;; ## Basics
 
 ;; The classic (and verbose) `loop` + `recur` example.
-(defn factorial-using-recur [x]
-  (loop [current x
+(defn factorial-using-recur [n]
+  (loop [current n
          next (dec current)
          total 1]
     (if (> current 1)
@@ -27,19 +27,19 @@
 ;; `reduce` `*`
 ;; on a
 ;; `range`
-(defn factorial-using-reduce [x]
-  (reduce * (range 1 (inc x))))
+(defn factorial-using-reduce [n]
+  (reduce * (range 1 (inc n))))
 
 ;; `apply` `*`
 ;; to a
 ;;`range`
-(defn factorial-using-apply-range [x]
-  (apply * (range 1 (inc x))))
+(defn factorial-using-apply-range [n]
+  (apply * (range 1 (inc n))))
 
 ;; `apply` `*`
 ;;  but `take`-ing from an `iterate`
-(defn factorial-using-apply-iterate [x]
-  (apply * (take x (iterate inc 1))))
+(defn factorial-using-apply-iterate [n]
+  (apply * (take n (iterate inc 1))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## "Code is data, data is code"
@@ -65,13 +65,13 @@
 ;;      (macroexpand `(factorial-function-macro 10))
 ;;      => (fn* ([] (clojure.core/* 1 2 3 4 5 6 7 8 9 10)))
 ;;
-(defmacro factorial-function-macro [x]
-  `(fn [] (* ~@(range 1 (inc x)))))
+(defmacro factorial-function-macro [n]
+  `(fn [] (* ~@(range 1 (inc n)))))
 
 ;; Here we illustrate Clojure [homoiconicity](http://en.wikipedia.org/wiki/Homoiconicity)
 ; using `eval` `cons` and `'` (quote)
-(defn factorial-using-eval-and-cons [x]
-  (eval (cons '* (range 1 (inc x)))))
+(defn factorial-using-eval-and-cons [n]
+  (eval (cons '* (range 1 (inc n)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## Parallel Computation
@@ -81,9 +81,9 @@
 ;; `partition-all` is just fantastic, by the way.
 ;;
 ;; Reminder: `map` is lazy, so force execution with `dorun` (effectively discarding the results).
-(defn factorial-using-ref-dosync [x psz]
+(defn factorial-using-ref-dosync [n psz]
   (let [result (ref 1)
-        parts (partition-all psz (range 1 (inc x)))
+        parts (partition-all psz (range 1 (inc n)))
         tasks (for [p parts]
       (future
         (Thread/sleep (rand-int 10))
@@ -98,9 +98,9 @@
 ;;
 ;; Also: per Joy of Clojure (§11.3.5) this is not the best use case for Agents (particularly
 ;; the `await` call at the end).
-(defn factorial-using-agent [x psz]
+(defn factorial-using-agent [n psz]
   (let [result (agent 1)
-        parts (partition-all psz (range 1 (inc x)))]
+        parts (partition-all psz (range 1 (inc n)))]
     (doseq [p parts]
       (send result #(apply * % p)))
     (await result)
@@ -112,8 +112,8 @@
 ;; If the previous code looked arduous, never fear. Enter: `pmap`
 ;;
 ;; Yes, it is `map` executed in parallel (lazily!) .
-(defn factorial-using-pmap-reduce [x psz]
-  (let [parts (partition-all psz (range 1 (inc x)))
+(defn factorial-using-pmap-reduce [n psz]
+  (let [parts (partition-all psz (range 1 (inc n)))
         sub-factorial-fn #(apply * %)]
     (reduce * (pmap sub-factorial-fn parts))))
 
@@ -122,15 +122,15 @@
 ;; Even though this works, it feels wrong having to jump through hoops with `defmacro` like this.
 ;
 ; `pmap` is more natural for this use-case.
-(defmacro factorial-using-pvalues-reduce [x psz]
-  (let [exprs (for [p (partition-all psz (range 1 (inc x)))]
+(defmacro factorial-using-pvalues-reduce [n psz]
+  (let [exprs (for [p (partition-all psz (range 1 (inc n)))]
                 (cons '* p))]
     `(fn [] (reduce * (pvalues ~@exprs)))))
 
 ;; `pvalues` calls a collection of zero-arg functions in parallel to create a lazy sequence.
 ;; Again, had to use `defmacro` to accomplish my goal.
-(defmacro factorial-using-pcalls-reduce [x psz]
-  (let [exprs (for [p (partition-all psz (range 1 (inc x)))]
+(defmacro factorial-using-pcalls-reduce [n psz]
+  (let [exprs (for [p (partition-all psz (range 1 (inc n)))]
                 `(fn [] (* ~@p)))]
     `(fn [] (reduce * (pcalls ~@exprs)))))
 
@@ -144,15 +144,15 @@
 ;;
 ;;     (trampoline (factorial-for-trampoline 5))
 ;;     => 120
-(defn factorial-for-trampoline [target]
+(defn factorial-for-trampoline [n]
   (letfn
-    [(next-fac-fn-or-value [target previous-step previous-value]
+    [(next-fac-fn-or-value [limit previous-step previous-value]
        (let [current-step (inc previous-step)
              current-value (* current-step previous-value)]
-         (if (= target current-step)
+         (if (= limit current-step)
            current-value
-           #(next-fac-fn-or-value target current-step current-value))))]
-    (next-fac-fn-or-value target 1 1)))
+           #(next-fac-fn-or-value limit current-step current-value))))]
+    (next-fac-fn-or-value n 1 1)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ### Multimethods
@@ -199,8 +199,8 @@
 
 ;; Elsewhere we wrote a plain old Java class with a static method that
 ;; computes factorials.  We can still re-use that legacy code via Clojure's Java interop.
-(defn factorial-using-javainterop [target]
-  (example.Factorial/calculate 5))
+(defn factorial-using-javainterop [n]
+  (example.Factorial/calculate n))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ### Taming Java Complexity
@@ -213,8 +213,8 @@
 ;;
 ;; Note: `doto` works well for "JavaBean" classes having a large number of `void` "setters".
 (import 'example.Factorial$Builder)
-(defn factorial-using-javainterop-and-pipeline [target]
-  (-> (Factorial$Builder.) (.factorial target) .build .compute))
+(defn factorial-using-javainterop-and-pipeline [n]
+  (-> (Factorial$Builder.) (.factorial n) .build .compute))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ### More on the Pipeline Macro
@@ -223,15 +223,15 @@
 
 ;; Executing the following at the REPL
 (comment
-  (clojure.walk/macroexpand-all '(-> (Factorial$Builder.) (.factorial target) .build .compute))
+  (clojure.walk/macroexpand-all '(-> (Factorial$Builder.) (.factorial n) .build .compute))
 )
 ;; outputs
 (comment
-  => (. (. (. (new Factorial$Builder) factorial target) build) compute)
+  => (. (. (. (new Factorial$Builder) factorial n) build) compute)
 )
 ;; which is equivalent to
 (comment
-  => (.compute (.build (.factorial (example.Factorial$Builder.) target)))
+  => (.compute (.build (.factorial (example.Factorial$Builder.) n)))
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -240,17 +240,17 @@
 ;; What if our Java team needs us to implement one of their API interfaces? Here
 ;; we use `reify` to generate a Java class that implements the `example.ValueComputer` interface
 ;; while re-using one of our functions for the implementation.
-(defn newFactorialComputer [x]
+(defn newFactorialComputer [n]
   (reify example.ValueComputer
-    (compute [this] (factorial-using-reduce x))))
+    (compute [this] (factorial-using-reduce n))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## Finally...
 
-;; Why not just use [Incanter](http://incanter.org/)? (DUH!)
+;; Why not just use [Incanter](http://incanter.org/)? (Duh!)
 (require '[incanter.core :only factorial])
-(defn factorial-from-incanter [x]
-  (incanter.core/factorial x))
+(defn factorial-from-incanter [n]
+  (incanter.core/factorial n))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; # Epilogue: HALL OF SHAME
@@ -263,20 +263,20 @@
 ;; After calling `(factorial-using-do-dotimes 5)` you will have a var named `a` pointing to a value
 ;; of `120`.  Unless another thread called the function concurrently, in which case who knows
 ;; what happened.
-(defn factorial-using-do-dotimes [x]
+(defn factorial-using-do-dotimes [n]
   (do
     (def a 1)
-    (dotimes [i x]
+    (dotimes [i n]
       (def a (* a (inc i)))))
   a)
 
 ;; This approach using `do` and `while` has the same correctness problem as above.
 ;; Now you have two vars in your namespace: `a` and `res`.
-(defn factorial-using-do-while [x]
+(defn factorial-using-do-while [n]
   (do
     (def a 0)
     (def res 1)
-    (while (< a x)
+    (while (< a n)
       (def a (inc a))
       (def res (* res a)))
     res))
@@ -285,10 +285,10 @@
 ;;
 ;; However, perhaps you could contrive an example using the `Factorial` struct from earlier and
 ;; `compare-and-set!`
-(defn factorial-using-atoms-while [x]
+(defn factorial-using-atoms-while [n]
   (let [a (atom 0)
         res (atom 1)]
-    (while (> x @a)
+    (while (> n @a)
       (swap! res * (swap! a inc)))
     @res))
 
@@ -302,13 +302,13 @@
 ;;     => (2 4 3 120 2 120 120 120 120 2)
 ;;     (for [x (range 10)] (factorial-using-agent-recursive 5))
 ;;     => (2 2 2 3 2 2 3 2 120 2)
-(defn factorial-using-agent-recursive [x]
+(defn factorial-using-agent-recursive [n]
   (let [a (agent 1)]
-    (letfn [(calc  [n limit total]
-               (if (< n limit)
-                 (let [next-n (inc n)]
+    (letfn [(calc  [current-n limit total]
+               (if (< current-n limit)
+                 (let [next-n (inc current-n)]
                    (send-off *agent* calc limit (* total next-n))
                    next-n)
                  total))]
-      (await (send-off a calc x 1)))
+      (await (send-off a calc n 1)))
     @a))
