5237cf956b6b11f86ad1eecee107769d901bf0b4
noahlz <nzucker@gmail.com>
2013-05-16 14:36:07 -0400

    upgraded clojure and incanter

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [incanter/incanter-core "1.3.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [incanter/incanter-core "1.5.0-SNAPSHOT"]])
+
